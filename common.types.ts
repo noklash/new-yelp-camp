d0862f98675a20c2bc59b85488f8a2291fc2d784
noklash <123834419+noklash@users.noreply.github.com>
2023-08-22 16:30:06 +0300

    session

---
+++ common.types.ts
@@ -0,0 +1,63 @@
+import { User, Session } from 'next-auth'
+
+export type FormState = {
+    title: string;
+    description: string;
+    image: string;
+    price: number;
+    website: string;
+    country: string;
+};
+
+export interface PostInterface {
+    title: string;
+    description: string;
+    image: string;
+    price: number;
+    website: string;
+    country: string;
+    id: string;
+    createdBy: {
+      name: string;
+      email: string;
+      avatarUrl: string;
+      id: string;
+    };
+}
+
+export interface UserProfile {
+    id: string;
+    name: string;
+    email: string;
+    description: string | null;
+    avatarUrl: string;
+    facebook: string | null;
+    linkedinUrl: string | null;
+    projects: {
+      edges: { node: PostInterface }[];
+      pageInfo: {
+        hasPreviousPage: boolean;
+        hasNextPage: boolean;
+        startCursor: string;
+        endCursor: string;
+      };
+    };
+}
+
+export interface SessionInterface extends Session {
+  user: User & {
+    id: string;
+    name: string;
+    email: string;
+    avatarUrl: string;
+  };
+}
+
+export interface PostForm {
+  title: string;
+  description: string;
+  image: string;
+  price: number;
+  website: string;
+  country: string;
+}
