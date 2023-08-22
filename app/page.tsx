254f5675e6525a2bfcf16192067cde06ee7b05bb
noklash <123834419+noklash@users.noreply.github.com>
2023-08-22 13:32:48 +0300

    home page done for mobile and desktop view

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,10 @@ import Link from "next/link";
 const Home = () => {
   // const screen = window.innerWidth
   return (
-    <div>
-          <div className="w-full m-4">
+    <div className="lg:flex overflow-hidden">
+          <div className="lg:w-1/2">
+
+          <div className="w-full m-4 ">
               <Link href="/">
                   <Image
                       src="/logo.svg"
@@ -16,10 +18,10 @@ const Home = () => {
               </Link>
           </div>
 
-          <div className="">
+          
 
           {/* { screen < 765 && <div> */}
-               <div className="w-full">
+               <div className="w-full lg:hidden">
                     <Image
                       className=""
                       src="/HeroImageMobile.jpg"
@@ -29,39 +31,39 @@ const Home = () => {
                       alt="hero image"
                     />
                </div>
-               <div className="p-6">
-                    <h3 className="header font-bold sm:text-4xl capitalize">Explore the best camps on earth</h3>
-                    <p className="description font-2xl">YelpCamp is a curated list of the best camping spots on Earth. Unfiltered and unbiased reviews.</p>
-                    <div className="flex py-2">
+               <div className="p-7 lg:mt-12">
+                    <h3 className="header font-bold text-2xl  lg:text-4xl capitalize">Explore the best camps on earth</h3>
+                    <p className="description font-2xl lg:text-2xl py-2">YelpCamp is a curated list of the best camping spots on Earth. Unfiltered and unbiased reviews.</p>
+                    <div className="flex py-3">
                         <Image
                           src="/CheckMark.svg"
                           alt="checkmark"
-                          width={20}
-                          height={20}
+                          width={30}
+                          height={30}
                         />
-                       <span className="px-3">Add your own camp suggestions.</span> 
+                       <span className="px-3 ">Add your own camp suggestions.</span> 
                     </div> 
-                    <div className="flex py-2">
+                    <div className="flex py-3">
                     <Image
                           src="/CheckMark.svg"
                           alt="checkmark"
-                          width={20}
-                          height={20}
+                          width={30}
+                          height={30}
                         />
-                       <span className="px-3 pt-1">Leave reviews and experiences.</span> 
+                       <span className="px-3 ">Leave reviews and experiences.</span> 
                     </div>
-                    <div className="flex pt-2 pb-3">
+                    <div className="flex pt-3 pb-6">
                     <Image
                           src="/CheckMark.svg"
                           alt="checkmark"
-                          width={20}
-                          height={20}
+                          width={30}
+                          height={30}
                         />
-                       <span className="px-3">See locations for all camps.</span> 
+                       <span className="px-3 ">See locations for all camps.</span> 
                     </div>
-                    <Link href="/campgrounds"><div className="w-40 bg-black text-white text-sm py-3 px-3 rounded capitalize">view campgrounds</div></Link>
+                    <Link href="/campgrounds" className=""><div className="w-40 bg-black text-white text-sm py-3 px-3 rounded capitalize">view campgrounds</div></Link>
   
-                    <div className="py-3">
+                    <div className="py-4">
                         <h5>Partnered with:</h5>
                         <div className="flex px-2">
                         <Image
@@ -88,10 +90,22 @@ const Home = () => {
                         </div>
                     </div>
                </div>
-            {/* </div>}  */}
+            
+              
 
           </div>
-        
+{/* this is for the desktop view */}
+                <div className="w-1/2 hidden lg:flex overflow-hidden ">
+                    <Image
+                      className=""
+                      src="/HeroImage.jpg"
+                      width={400}
+                      height={400}
+                      layout="responsive"
+                      alt="hero image"
+                      // objectFit="cover"
+                    />
+                </div>
     </div>
   )
 }
