88b8be07a03da0b810c3cc2eb2431f124e768926
noklash <123834419+noklash@users.noreply.github.com>
2023-09-19 09:44:32 +0300

    created dynamic page for the camps

---
+++ app/campgrounds/[id]/page.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+import camps from '../page'
+
+const camp = ( {params: { id} } : { params: {id: string} }) => {
+    
+   
+   
+  return (
+    <div>
+        <h1>{}</h1>
+
+    </div>
+  )
+}
+
+export default camp
