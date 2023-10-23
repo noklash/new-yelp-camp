48e063b746f5361540ff891c91a67c50b145fb5b
noklash <123834419+noklash@users.noreply.github.com>
2023-10-23 10:32:00 +0300

    updated next js

--- app/campgrounds/page.tsx
+++ app/campgrounds/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Image from 'next/image'
-import Link from 'next/link';
+// import Image from 'next/image'
+// import Link from 'next/link';
 
 import { PostInterface } from "@/common.types"
 import PostCard from "@/components/PostCard"
