fe7c0ef1dcae5da6c33732162afb637e41870408
noklash <123834419+noklash@users.noreply.github.com>
2024-01-14 21:40:36 +0100

    reinstalled grafbse

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,8 @@ const Home = () => {
 return (
   <div className='p-6 flex flex-col home overflow-y-clip'>
         <div className='flex flex-col mt-8 py-6 justify-center mx-auto'>
-      
           <AnimatedHome/>
         </div>
-          
-
     <div className='flex m-4 p-6 gap-8 text-white mx-auto '>
         <SignIn/>
     </div>
