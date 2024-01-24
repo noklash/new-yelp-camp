01c19747e02470bdae34d38fa7da57a15febff64
noklash <123834419+noklash@users.noreply.github.com>
2024-01-24 15:28:11 +0100

    moved grafbase.config to root folder

--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,42 +1,70 @@
-import { graph, config } from '@grafbase/sdk'
+import { config, connector, g, auth } from '@grafbase/sdk'
 
-// Welcome to Grafbase!
-//
-// Configure authentication, data sources, resolvers and caching for your GraphQL API.
+const mongodb = connector.MongoDB('MongoDB', {
+  url: g.env("MONGODB_API_URL"),
+  apiKey: g.env("MONGODB_API_KEY"),
+  dataSource: g.env("MONGODB_DATASOURCE"),
+  database: g.env("MONGODB_DATABASE")
+})
 
-const g = graph.Standalone()
 
-// Data Sources - https://grafbase.com/docs/connectors
-//
-// const pg = connector.Postgres('pg', { url: g.env('DATABASE_URL') })
-// g.datasource(pg)
 
-// Resolvers - https://grafbase.com/docs/resolvers
-//
-// g.query('helloWorld', {
-//   returns: g.string(),
-//   resolver: 'hello-world',
-// })
+// @ts-ignore
+// const User = 
+
+
+const post = g.type('post', {
+  title: g.string(),//.length({ min: 3 }),
+  description: g.string(), 
+  image: g.url(), 
+  country: g.string(),
+  createdBy: g.string(),
+})
+
+const user = g.type('user', {
+  name: g.string(),
+  email: g.string(),
+  avatarUrl: g.url(),
+  description: g.string(),
+  posts: g.string().list()
+})
+
+
+
+ mongodb.model('User', {
+  name: g.string().length({ min: 2, max: 100 }),
+  email: g.string().unique(),
+  avatarUrl: g.url(),
+  description: g.string().length({ min: 2, max: 1000 }).optional(), 
+  posts: g.ref(post).list().optional(),
+}).auth((rules) => {
+  rules.public().read()
+}).collection('users')
+
+// @ts-ignore
+// const Post = 
+mongodb.model('Post', {
+  title: g.string().length({ min: 3 }),
+  description: g.string(), 
+  image: g.url(), 
+  country: g.string(),
+  createdBy: g.ref(user).optional(),
+}).auth((rules) => {
+  rules.public().read()
+  rules.private().create().delete().update()
+}).collection('posts')
+
+g.datasource(mongodb)
+
+const jwt = auth.JWT({
+  issuer: 'grafbase',
+  secret:  g.env('NEXTAUTH_SECRET')
+})
 
 export default config({
-  graph: g,
-  // Authentication - https://grafbase.com/docs/auth
+  schema: g,
   auth: {
-    // OpenID Connect
-    // const oidc = auth.OpenIDConnect({ issuer: g.env('OIDC_ISSUER_URL') })
-    // providers: [oidc],
-    rules: (rules) => {
-      rules.public()
-    },
+    providers: [jwt],
+    rules: (rules) => rules.private()
   },
-  // Caching - https://grafbase.com/docs/graphql-edge-caching
-  // cache: {
-  //   rules: [
-  //     {
-  //       types: ['Query'], // Cache everything for 60 seconds
-  //       maxAge: 60,
-  //       staleWhileRevalidate: 60
-  //     }
-  //   ]
-  // }
 })
