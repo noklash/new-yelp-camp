65662991cc3fba6ee98b5d0ba321a9d607631bc4
noklash <123834419+noklash@users.noreply.github.com>
2024-01-24 10:16:33 +0100

    connector done

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,71 +1,40 @@
-// import { g, auth, config } from '@grafbase/sdk'
 
 
-// // @ts-ignore
-// const User = g.model('User', {
-//   name: g.string().length({ min: 2, max: 100 }),
-//   email: g.string().unique(),
-//   avatarUrl: g.url(),
-//   description: g.string().length({ min: 2, max: 1000 }).optional(), 
-//   posts: g.relation(() => Post).list().optional(),
-// }).auth((rules) => {
-//   rules.public().read()
-// })
+import { config, connector, g, auth } from '@grafbase/sdk'
 
-// // @ts-ignore
-// const Post = g.model('Post', {
-//   title: g.string().length({ min: 3 }),
-//   description: g.string(), 
-//   image: g.url(), 
-//   country: g.string(),
-//   createdBy: g.relation(() => User),
-// }).auth((rules) => {
-//   rules.public().read()
-//   rules.private().create().delete().update()
-// })
-
-// const jwt = auth.JWT({
-//   issuer: 'grafbase',
-//   secret:  g.env('NEXTAUTH_SECRET')
-// })
-
-// export default config({
-//   schema: g,
-//   auth: {
-//     providers: [jwt],
-//     rules: (rules) => rules.private()
-//   },
-// })
-
-// OLD ENDS
-
-// NEW STARTS
-
-import { config, connector, graph, auth } from '@grafbase/sdk'
-
-
-const g = graph.Standalone()
-
-
-
-const mongo = connector.MongoDB('MongoDB', {
-  url: g.env('MONGODB_API_URL'),
-  apiKey: g.env('MONGODB_API_KEY'),
-  dataSource: g.env('MONGODB_DATASOURCE'),
-  database: g.env('MONGODB_DATABASE'),
+const mongodb = connector.MongoDB('MongoDB', {
+  url: g.env("MONGODB_API_URL"),
+  apiKey: g.env("MONGODB_API_KEY"),
+  dataSource: g.env("MONGODB_DATASOURCE"),
+  database: g.env("MONGODB_DATABASE")
 })
 
-g.datasource(mongo)
 
-const User = g.model('User', {
+
+// @ts-ignore
+ mongodb.model('User', {
   name: g.string().length({ min: 2, max: 100 }),
   email: g.string().unique(),
   avatarUrl: g.url(),
   description: g.string().length({ min: 2, max: 1000 }).optional(), 
-  posts: g.relation(() => Post).list().optional(),
+  posts: g.ref('Post').list().optional(),
 }).auth((rules) => {
   rules.public().read()
-})
+}).collection('users')
+
+// @ts-ignore
+ mongodb.model('Post', {
+  title: g.string().length({ min: 3 }),
+  description: g.string(), 
+  image: g.url(), 
+  country: g.string(),
+  createdBy: g.ref('User'),
+}).auth((rules) => {
+  rules.public().read()
+  rules.private().create().delete().update()
+}).collection('posts')
+
+g.datasource(mongodb)
 
 const jwt = auth.JWT({
   issuer: 'grafbase',
@@ -73,9 +42,11 @@ const jwt = auth.JWT({
 })
 
 export default config({
-  graph: g,
-    auth: {
+  schema: g,
+  auth: {
     providers: [jwt],
     rules: (rules) => rules.private()
   },
 })
+
+
