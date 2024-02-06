988f643196ab26264e3911f6c194172e310793fc
noklash <123834419+noklash@users.noreply.github.com>
2024-02-06 23:56:03 +0100

    removed old

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,45 +1,4 @@
-// import { g, auth, config } from '@grafbase/sdk'
 
-
-// // @ts-ignore
-// const User = g.model('User', {
-//   name: g.string().length({ min: 2, max: 100 }),
-//   email: g.string().unique(),
-//   avatarUrl: g.url(),
-//   description: g.string().length({ min: 2, max: 1000 }).optional(), 
-//   posts: g.relation(() => Post).list().optional(),
-// }).auth((rules) => {
-//   rules.public().read()
-// })
-
-// // @ts-ignore
-// const Post = g.model('Post', {
-//   title: g.string().length({ min: 3 }),
-//   description: g.string(), 
-//   image: g.url(), 
-//   country: g.string(),
-//   createdBy: g.relation(() => User),
-// }).auth((rules) => {
-//   rules.public().read()
-//   rules.private().create().delete().update()
-// })
-
-// const jwt = auth.JWT({
-//   issuer: 'grafbase',
-//   secret:  g.env('NEXTAUTH_SECRET')
-// })
-
-// export default config({
-//   schema: g,
-//   auth: {
-//     providers: [jwt],
-//     rules: (rules) => rules.private()
-//   },
-// })
-
-// OLD ENDS
-
-// NEW STARTS
 
 import { config, connector, g, auth } from '@grafbase/sdk'
 
